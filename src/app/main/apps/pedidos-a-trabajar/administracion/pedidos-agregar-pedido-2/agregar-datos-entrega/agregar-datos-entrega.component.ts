2f131a638a9079933babee94cd01c91f22099835
marianorobles92 <49004892+marianorobles92@users.noreply.github.com>
2020-08-19 12:05:45 -0300

    Agregar pedido 2. modal con combos

--- src/app/main/apps/pedidos-a-trabajar/administracion/pedidos-agregar-pedido-2/agregar-datos-entrega/agregar-datos-entrega.component.ts
+++ src/app/main/apps/pedidos-a-trabajar/administracion/pedidos-agregar-pedido-2/agregar-datos-entrega/agregar-datos-entrega.component.ts
@@ -1,19 +1,206 @@
-import { Component, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { HttpErrorResponse } from '@angular/common/http';
+import { PedidosListaService } from '../../pedidos-lista/pedidos-lista.service';
+import { Router } from '@angular/router';
+import { ModalErrorComponent } from 'app/shared/modal-error/modal-error.component';
 
 @Component({
   selector: 'app-agregar-datos-entrega',
   templateUrl: './agregar-datos-entrega.component.html',
   styleUrls: ['./agregar-datos-entrega.component.scss']
 })
-export class AgregarDatosEntregaComponent {
+export class AgregarDatosEntregaComponent implements OnInit{
 
   constructor(
     public dialogRef: MatDialogRef<AgregarDatosEntregaComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {}
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private _pedidosListaService: PedidosListaService,
+    private _dialog: MatDialog,
+    private _router: Router) {}
 
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
+
+    filtroProvincias: any;
+    selectedProvincia: any = 1;
+
+    filtroLocalidades: any;
+    selectedLocalidad: any = 1402;
+    selectedCodigoPostal: any;
+
+    filtroTurnos: any;
+    selectedTurno: any = 0;
+
+    ngOnInit(): void {
+      this.getfiltros();
+    }
+
+    onNoClick(): void {
+      this.dialogRef.close();
+    }
+
+    getfiltros(){
+
+      this._pedidosListaService.getAllTurnos().subscribe(params => {
+        this.filtroTurnos = params.datos;
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log("Client-side error");
+        } else {
+          let errStatus = err.status
+          if (errStatus == 0){
+            let titulo = 'Error de Servidor';
+            let mensaje = "Por favor comunicarse con Sistemas";
+            this.mostrarError(errStatus, titulo, mensaje);
+          } else {
+            let titulo = 'Error al cargar filtros';
+            let mensaje = err.error.message.toString();
+            this.mostrarError(errStatus, titulo, mensaje);
+          }
+        }
+      })
+
+
+      this._pedidosListaService.getAllProvincias().subscribe(params => {
+        this.filtroProvincias = params.datos;
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log("Client-side error");
+        } else {
+          let errStatus = err.status
+          if (errStatus == 0){
+            let titulo = 'Error de Servidor';
+            let mensaje = "Por favor comunicarse con Sistemas";
+            this.mostrarError(errStatus, titulo, mensaje);
+          } else {
+            let titulo = 'Error al cargar filtros';
+            let mensaje = err.error.message.toString();
+            this.mostrarError(errStatus, titulo, mensaje);
+          }
+        }
+      })
+  
+      this._pedidosListaService.getAllLocalidades().subscribe(params => {
+        this.filtroLocalidades = params.datos;
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log("Client-side error");
+        } else {
+          let errStatus = err.status
+          if (errStatus == 0){
+            let titulo = 'Error de Servidor';
+            let mensaje = "Por favor comunicarse con Sistemas";
+            this.mostrarError(errStatus, titulo, mensaje);
+          } else {
+            let titulo = 'Error al cargar filtros';
+            let mensaje = err.error.message.toString();
+            this.mostrarError(errStatus, titulo, mensaje);
+          }
+        }
+      })
+    };
+
+
+    selectProvincia(event: Event) {
+      this.selectedProvincia = (event.target as HTMLSelectElement).value;
+      if(this.selectedProvincia > 0){
+        this.selectedLocalidad = 0;
+        this._pedidosListaService.getAllLocalidadesPorProvincia(this.selectedProvincia).subscribe(params => {
+          this.filtroLocalidades = params.datos;
+        },
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            console.log("Client-side error");
+          } else {
+            let errStatus = err.status
+            if (errStatus == 0){
+              let titulo = 'Error de Servidor';
+              let mensaje = "Por favor comunicarse con Sistemas";
+              this.mostrarError(errStatus, titulo, mensaje);
+            } else {
+              let titulo = 'Error al cargar filtros';
+              let mensaje = err.error.message.toString();
+              this.mostrarError(errStatus, titulo, mensaje);
+            }
+          }
+        })
+      } else {
+        this.selectedLocalidad = 0;
+        this._pedidosListaService.getAllLocalidades().subscribe(params => {
+          this.filtroLocalidades = params.datos;
+        },
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            console.log("Client-side error");
+          } else {
+            let errStatus = err.status
+            if (errStatus == 0){
+              let titulo = 'Error de Servidor';
+              let mensaje = "Por favor comunicarse con Sistemas";
+              this.mostrarError(errStatus, titulo, mensaje);
+            } else {
+              let titulo = 'Error al cargar filtros';
+              let mensaje = err.error.message.toString();
+              this.mostrarError(errStatus, titulo, mensaje);
+            }
+          }
+        })
+      }
+    }
+  
+    selectLocalidad(event: Event) {
+      this.selectedLocalidad = (event.target as HTMLSelectElement).value;
+      // this.selectedCodigoPostal = (event.target as HTMLSelectElement);
+      console.log((event.target as HTMLSelectElement).value);
+      if(this.selectedLocalidad > 0){
+        this._pedidosListaService.getProvinciaPorLocalidad(this.selectedLocalidad).subscribe( params => {
+          this.selectedProvincia = params.id;
+          console.log("Provincia: "+this.selectedProvincia);
+        },
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            console.log("Client-side error");
+          } else {
+            let errStatus = err.status
+            if (errStatus == 0){
+              let titulo = 'Error de Servidor';
+              let mensaje = "Por favor comunicarse con Sistemas";
+              this.mostrarError(errStatus, titulo, mensaje);
+            } else {
+              let titulo = 'Error al cargar filtros';
+              let mensaje = err.error.message.toString();
+              this.mostrarError(errStatus, titulo, mensaje);
+            }
+          }
+        })
+      }
+    }
+
+    selectTurno(event: Event) {
+      this.selectedTurno = (event.target as HTMLSelectElement).value;
+      console.log(this.selectedTurno);
+    }
+
+
+    mostrarError(errStatus, titulo, mensaje){
+      const dialogRef = this._dialog.open( ModalErrorComponent, { 
+        data: {
+          titulo: titulo,
+          mensaje: mensaje
+        } 
+      });
+  
+      dialogRef.afterClosed()
+        .subscribe( () => {
+            if (errStatus != 0) {
+
+              
+            } else {
+              this._router.navigate(['']);
+            }
+        });
+    }
 
 }
