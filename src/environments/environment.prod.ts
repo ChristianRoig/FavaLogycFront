1ae40f098c23505e3ade3f3abc5551dae16e9585
marianorobles92 <49004892+marianorobles92@users.noreply.github.com>
2020-08-26 14:05:56 -0300

    Se cambia index.html y otro archivo mas para que se gemere correctamente el dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     hmr       : false,
-    server    : 'http://192.168.100.191:8080/',
-    // server    : 'http://192.168.0.6:8080/',
+    // server    : 'http://192.168.100.191:8080/',
+    server    : 'http://localhost:8080/',
     baseUrl   : 'api_favalogyc/'
 };
