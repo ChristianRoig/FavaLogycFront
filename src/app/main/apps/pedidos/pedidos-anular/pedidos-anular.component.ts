456dd26d09e7c625af35a6ee04b40cf9c1ba5f14
PicassoOctavio <octavio.picasso.ma@gmail.com>
2021-02-24 15:02:56 -0300

    correccion en la navegacion despues de anular un pedido

--- src/app/main/apps/pedidos/pedidos-anular/pedidos-anular.component.ts
+++ src/app/main/apps/pedidos/pedidos-anular/pedidos-anular.component.ts
@@ -34,24 +34,12 @@ export interface Articulos {
   etapa: string;
 }
 
-// const ELEMENT_DATA: PeriodicElement[] = [
-//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-//   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-//   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-//   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-//   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-//   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-// ];
-
 const ELEMENT_DATA_ARTICULOS: Articulos[] = [
   {lote: 1,codigoArticulo: "HCTZCAB030",nombre: "CTZ CALEFACTOR 6000	",          descripcion: "12 MESES DE GARANTIA",cantidad: 1,estado: "INICIAL",etapa: "INICIAL"},
   {lote: 1,codigoArticulo: "HCTZACC010",nombre: "CTZ ACCESORIO TB	",              descripcion: "12 MESES DE GARANTIA",cantidad: 1,estado: "INICIAL",etapa: "INICIAL"},
   {lote: 1,codigoArticulo: "HMOUFRE050",nombre: "MOULINEX FREIDORA AF134DAR/D59	",descripcion: "12 MESES DE GARANTIA",cantidad: 1,estado: "INICIAL",etapa: "INICIAL"}
 ];
+
 @Component({
   selector: 'app-pedidos-anular',
   templateUrl: './pedidos-anular.component.html',
@@ -160,9 +148,7 @@ export class PedidosAnularComponent implements OnInit {
     dialogRef.afterClosed()
       .subscribe( () => {
         if (errStatus != 0) {  
-
           this.volver();
-          
         } else {
           this._router.navigate(['']);
         }
@@ -211,7 +197,7 @@ export class PedidosAnularComponent implements OnInit {
   }
 
   volver(){
-    let ruta = `apps/pedidos/pedidos-lista`;
+    let ruta = `apps/pedidos/ver-pedido/${ this.idCabecera }`;
     this._router.navigate([ruta]);
   }
 
@@ -219,7 +205,6 @@ export class PedidosAnularComponent implements OnInit {
 
 
   armarArrarIds(){
-
     for (let elemento of this.selection.selected){
       this.toDelete.push(elemento.id);
     }
@@ -250,7 +235,7 @@ export class PedidosAnularComponent implements OnInit {
         }
       }
     );
-
+    
   }
 
 
