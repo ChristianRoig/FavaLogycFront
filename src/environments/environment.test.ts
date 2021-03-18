54161de03ae6a63a2ba77065bb72c0cf57ab8ce0
PicassoOctavio <octavio.picasso.ma@gmail.com>
2021-03-18 09:58:47 -0300

    environment test

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     hmr       : false,
     server    : 'http://192.168.100.191:8080/',
-    baseUrl   : 'api_favalogyc/'
+    baseUrl   : 'api_favalogyc_testing/'
 };
