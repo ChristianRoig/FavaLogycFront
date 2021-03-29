41fbc1676f3377e3b96501e738f800bacef96fbe
Christian Roig <ch.roig@gmail.com>
2021-03-29 09:35:23 -0300

    ajustes

--- src/app/main/apps/dashboard/project.module.ts
+++ src/app/main/apps/dashboard/project.module.ts
@@ -19,7 +19,7 @@ import { ProjectDashboardService } from './project.service';
 
 const routes: Routes = [
     {
-        path     : 'dash',
+        path     : 'tablero',
         component: ProjectDashboardComponent,
         /* resolve  : {
             data: ProjectDashboardService
