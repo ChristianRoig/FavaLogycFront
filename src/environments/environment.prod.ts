6ac22c2ee5e1f24768f6e6c53cfdb437366b0e25
Christian Roig <ch.roig@gmail.com>
2021-06-01 14:55:40 -0300

    environments y selector

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     hmr       : false,
-    server    : 'http://192.168.100.191:8080/',
-    baseUrl   : 'api_favalogyc/'
+    server    : 'http://192.168.100.117:28080/',
+    baseUrl   : 'api_favalogyc_frontend/'
 };
